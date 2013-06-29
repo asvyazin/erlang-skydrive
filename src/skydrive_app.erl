4c672b811141399624144b92c8c4cbc1e43ddfa7
Alexander Svyazin <guybrush@live.ru>
2013-06-29 22:29:41 +0600

    rebar skeleton, query_string parser

---
+++ src/skydrive_app.erl
@@ -0,0 +1,16 @@
+-module(skydrive_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    skydrive_sup:start_link().
+
+stop(_State) ->
+    ok.
---
+++ src/skydrive_util.erl
@@ -0,0 +1,17 @@
+-module(skydrive_util).
+-author('Alexander Svyazin <guybrush@live.ru>').
+
+-export([auth_url/3, parse_auth_code/1]).
+
+-define(AUTH_HOST, "https://login.live.com").
+-define(AUTH_PATH_BASE, "oauth20_authorize.srf").
+
+auth_url(ClientId, Scopes, RedirectUrl) ->
+    ScopeArg = string:join(Scopes, " "),
+    io_lib:format("~s/~s?client_id=~s&scope=~s&response_type=code&redirect_uri=~s",
+		  [?AUTH_HOST, ?AUTH_PATH_BASE, ClientId, edoc_lib:escape_uri(ScopeArg), edoc_lib:escape_uri(RedirectUrl)]).
+
+parse_auth_code(RedirectedUrl) ->
+    {ok, {_, _, _, _, _, Query}} = http_uri:parse(RedirectedUrl),
+    url_query_string:parse(Query),
+    1.
