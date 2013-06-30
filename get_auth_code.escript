d98ab91fd866981c767a10a9de0bc7e1fe8b7883
Alexander Svyazin <guybrush@live.ru>
2013-06-30 09:43:01 +0600

    small cleanup

--- get_auth_code.escript
+++ get_auth_code.escript
@@ -3,7 +3,8 @@
 %%! -pz ebin
 
 main([ClientId]) ->
-    AuthUrl = skydrive_util:auth_url(ClientId, ["wl.skydrive", "wl.offline_access"], "https://login.live.com/oauth20_desktop.srf"),
+    Scopes = ["wl.skydrive", "wl.offline_access", "wl.signin", "wl.skydrive_update"],
+    AuthUrl = skydrive_util:auth_url(ClientId, Scopes, "https://login.live.com/oauth20_desktop.srf"),
     RedirectedUrl = io:get_line(io_lib:format("copy this URL into the browser's address line: ~s~ncopy URL from address line after login and redirect here:", [AuthUrl])),
     {ok, AuthCode} = skydrive_util:parse_auth_code(RedirectedUrl),
     io:format("auth code: ~s~n", [AuthCode]).
--- src/skydrive_util.erl
+++ src/skydrive_util.erl
@@ -1,11 +1,12 @@
 -module(skydrive_util).
 -author('Alexander Svyazin <guybrush@live.ru>').
 
--export([auth_url/3, parse_auth_code/1, get_token/4]).
+-export([auth_url/3, parse_auth_code/1, get_token/4, get_token_url/0, get_token_body/4, request/2]).
 
 -define(AUTH_HOST, "https://login.live.com").
 -define(AUTH_PATH_BASE, "oauth20_authorize.srf").
 -define(AUTH_TOKEN_PATH, "oauth20_token.srf").
+-define(API_BASE, "https://apis.live.net/v5.0").
 
 auth_url(ClientId, Scopes, RedirectUrl) ->
     ScopeArg = string:join(Scopes, " "),
@@ -18,10 +19,20 @@ parse_auth_code(RedirectedUrl) ->
     Params = url_query_string:parse(Query),
     {ok, proplists:get_value(<<"code">>, Params)}.
 
-get_token(ClientId, ClientSecret, RedirectUrl, AuthCode) ->
-    TokenUrl = io_lib:format("~s/~s", [?AUTH_HOST, ?AUTH_TOKEN_PATH]),
+get_token_url() ->
+    lists:flatten(io_lib:format("~s/~s", [?AUTH_HOST, ?AUTH_TOKEN_PATH])).
+
+get_token_body(ClientId, ClientSecret, RedirectUrl, AuthCode) ->
     TokenReqBody = io_lib:format("client_id=~s&redirect_uri=~s&client_secret=~s&code=~s&grant_type=authorization_code",
 				 [edoc_lib:escape_uri(ClientId), edoc_lib:escape_uri(RedirectUrl), edoc_lib:escape_uri(ClientSecret), edoc_lib:escape_uri(AuthCode)]),
-    {ok, {{_, 200, _}, _, TokenRespBody}} = httpc:request(post, {lists:flatten(TokenUrl), [], "application/x-www-form-urlencoded", iolist_to_binary(TokenReqBody)}, [], []),
+    iolist_to_binary(TokenReqBody).
+
+get_token(ClientId, ClientSecret, RedirectUrl, AuthCode) ->
+    TokenReqBody = get_token_body(ClientId, ClientSecret, RedirectUrl, AuthCode),
+    {ok, {{_, 200, _}, _, TokenRespBody}} = httpc:request(post, {get_token_url(), [], "application/x-www-form-urlencoded", TokenReqBody}, [], []),
     TokenRespJson = mochijson2:decode(TokenRespBody),
     io:format("~p~n", [TokenRespJson]).
+
+request(Token, Query) ->
+    RequestUrl = lists:flatten(io_lib:format("~s/~s?access_token=~s", [?API_BASE, Query, edoc_lib:escape_uri(Token)])),
+    httpc:request(RequestUrl).
