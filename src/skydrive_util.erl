52f6ff42e18b196569f2b8bc73793bab081cae7a
Alexander Svyazin <guybrush@live.ru>
2013-06-30 23:48:07 +0600

    refresh token url and body, small refactoring

--- src/skydrive_util.erl
+++ src/skydrive_util.erl
@@ -1,16 +1,24 @@
 -module(skydrive_util).
 -author('Alexander Svyazin <guybrush@live.ru>').
 
--export([auth_url/3, auth_parse_code/1, token_req_url/0, token_req_body/4]).
+-export([auth_url/3, auth_parse_code/1, token_req_url/0, token_req_body/4, token_refresh_url/0, token_refresh_body/4]).
 
 -define(AUTH_HOST, "https://login.live.com").
 -define(AUTH_PATH_BASE, "oauth20_authorize.srf").
 -define(AUTH_TOKEN_PATH, "oauth20_token.srf").
 
+format_qs(Params) ->
+    FParams = [format_param(P) || P <- Params],
+    string:join(FParams, "&").
+
+format_param({Name, Value}) ->
+    io_lib:format("~s=~s", Name, edoc_lib:escape_uri(Value)).
+
 auth_url(ClientId, Scopes, RedirectUrl) ->
-    ScopeArg = string:join(Scopes, " "),
-    Url = io_lib:format("~s/~s?client_id=~s&scope=~s&response_type=code&redirect_uri=~s",
-			[?AUTH_HOST, ?AUTH_PATH_BASE, edoc_lib:escape_uri(ClientId), edoc_lib:escape_uri(ScopeArg), edoc_lib:escape_uri(RedirectUrl)]),
+    Query = format_qs([{client_id, ClientId},
+		       {scope, string:join(Scopes, " ")},
+		       {redirect_uri, RedirectUrl}]),
+    Url = io_lib:format("~s/~s?~s", [?AUTH_HOST, ?AUTH_PATH_BASE, Query]),
     lists:flatten(Url).
 
 auth_parse_code(RedirectedUrl) ->
@@ -22,6 +30,20 @@ token_req_url() ->
     lists:flatten(io_lib:format("~s/~s", [?AUTH_HOST, ?AUTH_TOKEN_PATH])).
 
 token_req_body(ClientId, ClientSecret, RedirectUrl, AuthCode) ->
-    TokenReqBody = io_lib:format("client_id=~s&redirect_uri=~s&client_secret=~s&code=~s&grant_type=authorization_code",
-				 [edoc_lib:escape_uri(ClientId), edoc_lib:escape_uri(RedirectUrl), edoc_lib:escape_uri(ClientSecret), edoc_lib:escape_uri(AuthCode)]),
+    TokenReqBody = format_qs([{client_id, ClientId},
+			      {redirect_uri, RedirectUrl},
+			      {client_secret, ClientSecret},
+			      {grant_type, authorization_code},
+			      {code, AuthCode}]),
+    iolist_to_binary(TokenReqBody).
+
+token_refresh_url() ->
+    token_req_url().
+
+token_refresh_body(ClientId, ClientSecret, RedirectUrl, RefreshToken) ->
+    TokenReqBody = format_qs([{client_id, ClientId},
+			      {redirect_uri, RedirectUrl},
+			      {client_secret, ClientSecret},
+			      {grant_type, refresh_token},
+			      {refresh_token, RefreshToken}]),
     iolist_to_binary(TokenReqBody).
