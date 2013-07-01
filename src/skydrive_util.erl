135d4e34623768d0d720ab12f9fd34c1596f7c0f
Alexander Svyazin <guybrush@live.ru>
2013-07-02 00:27:17 +0600

    fixed query string formatting, added skydrive_util:desktop_url()

--- src/skydrive_util.erl
+++ src/skydrive_util.erl
@@ -1,23 +1,29 @@
 -module(skydrive_util).
 -author('Alexander Svyazin <guybrush@live.ru>').
 
--export([auth_url/3, auth_parse_code/1, token_req_url/0, token_req_body/4, token_refresh_url/0, token_refresh_body/4]).
+-export([auth_url/3, auth_parse_code/1, token_req_url/0, token_req_body/4, token_refresh_url/0, token_refresh_body/4, desktop_url/0]).
 
 -define(AUTH_HOST, "https://login.live.com").
 -define(AUTH_PATH_BASE, "oauth20_authorize.srf").
 -define(AUTH_TOKEN_PATH, "oauth20_token.srf").
+-define(AUTH_DESKTOP_PATH, "oauth20_desktop.srf").
 
 format_qs(Params) ->
     FParams = [format_param(P) || P <- Params],
     string:join(FParams, "&").
 
 format_param({Name, Value}) ->
-    io_lib:format("~s=~s", Name, edoc_lib:escape_uri(Value)).
+    io_lib:format("~s=~s", [Name, escape_uri(Value)]).
+
+escape_uri(Value) when is_atom(Value) -> Value;
+escape_uri(Value) when is_binary(Value) -> escape_uri(binary_to_list(Value));
+escape_uri(Value) -> edoc_lib:escape_uri(Value).
 
 auth_url(ClientId, Scopes, RedirectUrl) ->
     Query = format_qs([{client_id, ClientId},
 		       {scope, string:join(Scopes, " ")},
-		       {redirect_uri, RedirectUrl}]),
+		       {redirect_uri, RedirectUrl},
+		       {response_type, code}]),
     Url = io_lib:format("~s/~s?~s", [?AUTH_HOST, ?AUTH_PATH_BASE, Query]),
     lists:flatten(Url).
 
@@ -47,3 +53,6 @@ token_refresh_body(ClientId, ClientSecret, RedirectUrl, RefreshToken) ->
 			      {grant_type, refresh_token},
 			      {refresh_token, RefreshToken}]),
     iolist_to_binary(TokenReqBody).
+
+desktop_url() ->
+    lists:flatten(io_lib:format("~s/~s", [?AUTH_HOST, ?AUTH_DESKTOP_PATH])).
